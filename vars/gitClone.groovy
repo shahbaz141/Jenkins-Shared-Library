47cab7fcae5a3718c9d601bbe7de36d65cbabf98
Shahbaz Hussain <105984933+shahbaz141@users.noreply.github.com>
2024-10-10 00:14:02 +0500

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,4 +1,4 @@
-def call(string url,string branch){
+def call(String url,String branch){
   echo "code is being pulled from GitHub"
   // Correct git step for pulling from the repository
   git url: ${url}, branch:${branch} 
