e54fb742023aacf28832b3961767c0fbb7855077
Shahbaz Hussain <105984933+shahbaz141@users.noreply.github.com>
2024-10-17 22:47:45 +0500

    Update loginDockerPush.groovy

--- vars/loginDockerPush.groovy
+++ vars/loginDockerPush.groovy
@@ -1,10 +1,9 @@
-def call(String id){
-   echo "pushing the image to the docker hub repo"
-  withCredentials([usernamePassword(credentialsId:"${id}", passwordVariable: 'password', usernameVariable: 'username')])
-{
-                 sh "docker login -u ${env.username} -p ${env.password}
-                sh "docker image tag notesapp:latest shahbaz141/notesapp:latest 
-                sh "docker push shahbaz141/notesapp:latest"
-                echo "login sucessfull and image pushed on the dockerhub"
-  }
+def call(String id) {
+    echo "Pushing the image to the Docker Hub repo"
+    withCredentials([usernamePassword(credentialsId: "${id}", passwordVariable: 'password', usernameVariable: 'username')]) {
+        sh "docker login -u ${env.username} -p ${env.password}"
+        sh "docker image tag notesapp:latest shahbaz141/notesapp:latest"
+        sh "docker push shahbaz141/notesapp:latest"
+        echo "Login successful and image pushed to Docker Hub"
+    }
 }
